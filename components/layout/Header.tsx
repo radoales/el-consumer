5ee8a535058b1d3af1edb7aff58315af97cda59b
radoslav <naydenov.radoslav@gmail.com>
2022-12-23 16:53:54 +0100

    add logo

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image"
 import styles from "../../styles/layout/header.module.scss"
 import Navbar from "./Navbar"
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      <Image src={"/logo.png"} width={30} height={30} alt={"logo"} />
       <Navbar />
     </div>
   )
