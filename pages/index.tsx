4494e57d56478d23eaea70ae11fa009a5b6c7988
radoslav <naydenov.radoslav@gmail.com>
2022-12-23 20:13:45 +0100

    add get price func

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,44 @@
 import { useEffect, useState } from "react"
 import { useGetData } from "../hooks/data"
-import { getCurrentHourPrice } from "../utils/helpers"
+import { getHourPrice, getPrice } from "../utils/helpers"
 import styles from "../styles/home/index.module.scss"
+import Input from "antd/es/input/Input"
+import { Slider } from "antd"
 
 export default function Home() {
   const { data } = useGetData()
   const [currentPrice, setCurrentPrice] = useState<number>()
+  const [slider, setSlider] = useState<number[]>([
+    new Date().getHours(),
+    new Date().getHours() + 2
+  ])
 
   useEffect(() => {
     if (data) {
-      setCurrentPrice(getCurrentHourPrice(data).DKK_per_kWh)
+      setCurrentPrice(getHourPrice(data, slider[0]).DKK_per_kWh)
+      console.log("s", getPrice(data, slider))
     }
-  }, [data])
-
-  console.log("data", data)
+  }, [data, slider])
 
   return (
     <div className={styles.home}>
       {data && (
-        <div className={styles.home__currentPriceBox}>
-          <div className={styles.home__currentPriceBox__price}>
-            {currentPrice?.toFixed(2)} dkk
+        <>
+          <div className={styles.home__currentPriceBox}>
+            <div className={styles.home__currentPriceBox__price}>
+              {currentPrice?.toFixed(2)} dkk
+            </div>
+            <div>price per kw/h</div>
           </div>
-          <div>price per kw/h</div>
-        </div>
+          <div className={styles.home__form}>
+            <Slider
+              range={{ draggableTrack: true }}
+              defaultValue={[slider[0], 30]}
+              max={23}
+              onChange={(e: number[]) => setSlider(e)}
+            />
+          </div>
+        </>
       )}
     </div>
   )
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,17 @@
 import { HourPrice } from "../types/elpris"
 
-export const getCurrentHourPrice = (data: HourPrice[]) => {
-  return data.filter(
-    (item) => new Date(item.time_start).getHours() === new Date().getHours()
-  )[0]
+export const getHourPrice = (data: HourPrice[], hour: number) => {
+  return data.filter((item) => new Date(item.time_start).getHours() === hour)[0]
+}
+
+export const getPrice = (data: HourPrice[], timeWindow: number[]) => {
+  const hourPrices = data
+    .filter(
+      (item) =>
+        new Date(item.time_start).getHours() > timeWindow[0] &&
+        new Date(item.time_start).getHours() < timeWindow[1]
+    )
+    .map((item) => item.DKK_per_kWh)
+
+  console.log("hourPrices", hourPrices)
 }
