256487eeafd4b5dfa55f5c845db84aa25c6f5ffb
radoslav <naydenov.radoslav@gmail.com>
2023-01-06 17:53:32 +0100

    add logos

--- utils/deviceslist.tsx
+++ utils/deviceslist.tsx
@@ -1,4 +1,5 @@
 import WashingMachine from "../assets/icons/washing-machine.svg"
+import Oven from "../assets/icons/oven.svg"
 
 export const DEVICES = [
   {
@@ -14,5 +15,12 @@ export const DEVICES = [
     consumption: 0.6,
     averageUsageHours: 2,
     icon: <WashingMachine width={25} height={25} fill='#000' />
+  },
+  {
+    id: 3,
+    name: "Oven",
+    consumption: 2.5,
+    averageUsageHours: 1,
+    icon: <Oven width={25} height={25} fill='#000' />
   }
 ]
