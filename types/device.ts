e7f3fd3c207741a743dc92a793b34133848b3e21
radoslav <naydenov.radoslav@gmail.com>
2023-01-08 15:16:20 +0100

    add new devices

--- types/device.ts
+++ types/device.ts
@@ -1,6 +1,9 @@
+import { DeviceCategory } from "../utils/deviceslist"
+
 export interface Device {
   id: number
   name: string
   consumption: number
   averageUsageHours: number
+  category: DeviceCategory
 }
--- utils/deviceslist.tsx
+++ utils/deviceslist.tsx
@@ -1,34 +1,74 @@
 import WashingMachine from "../assets/icons/washing-machine.svg"
 import Oven from "../assets/icons/oven.svg"
 import Stove from "../assets/icons/stove.svg"
+import Dishwasher from "../assets/icons/dishwasher.svg"
+import Kitchen from "../assets/icons/kitchen.svg"
+import Bathroom from "../assets/icons/bathroom.svg"
+import Refrigerator from "../assets/icons/refrigerator.svg"
+
+export enum DeviceCategory {
+  KITCHEN = "kitchen",
+  BATHROOM = "bathroom"
+}
+
+export const DEVICE_CATEGORIES = [
+  {
+    name: DeviceCategory.KITCHEN,
+    icon: <Kitchen width={25} height={25} fill='#000' />
+  },
+  {
+    name: DeviceCategory.BATHROOM,
+    icon: <Bathroom width={25} height={25} fill='#000' />
+  }
+]
 
 export const DEVICES = [
   {
     id: 1,
-    name: "Drier",
+    name: "Dryer",
     consumption: 0.9,
     averageUsageHours: 3,
-    icon: <WashingMachine width={25} height={25} fill='#000' />
+    category: DeviceCategory.BATHROOM,
+    icon: <WashingMachine width={40} height={40} fill='#000' />
   },
   {
     id: 2,
     name: "Washing machine",
     consumption: 0.6,
     averageUsageHours: 2,
-    icon: <WashingMachine width={25} height={25} fill='#000' />
+    category: DeviceCategory.BATHROOM,
+    icon: <WashingMachine width={40} height={40} fill='#000' />
   },
   {
     id: 3,
     name: "Oven",
     consumption: 2.5,
     averageUsageHours: 1,
-    icon: <Oven width={25} height={25} fill='#000' />
+    category: DeviceCategory.KITCHEN,
+    icon: <Oven width={40} height={40} fill='#000' />
   },
   {
     id: 4,
     name: "Stove",
     consumption: 1.5,
     averageUsageHours: 1,
-    icon: <Stove width={25} height={25} fill='#000' />
+    category: DeviceCategory.KITCHEN,
+    icon: <Stove width={40} height={40} fill='#000' />
+  },
+  {
+    id: 5,
+    name: "Dishwasher",
+    consumption: 1.3,
+    averageUsageHours: 2,
+    category: DeviceCategory.KITCHEN,
+    icon: <Dishwasher width={40} height={40} fill='#000' />
+  },
+  {
+    id: 6,
+    name: "Refrigerator",
+    consumption: 0.3,
+    averageUsageHours: 24,
+    category: DeviceCategory.KITCHEN,
+    icon: <Refrigerator width={40} height={40} fill='#000' />
   }
 ]
