2b2f7cec96a4748264c5b528a304d5a2d438b63c
radoslav <naydenov.radoslav@gmail.com>
2023-01-11 19:51:25 +0100

    add localstorrage for region state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,32 @@
 import { useGetPrices } from "../hooks/prices"
 import styles from "../styles/home/index.module.scss"
 import PriceCalculator from "../components/layout/priceCalculator"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { regionDenmark } from "../utils/constants"
-import Denmark from "../components/layout/map"
+import { localStorageKeys, useLocalStorage } from "../hooks/localStorage"
 
 export default function Home() {
-  const [region, setRegion] = useState<regionDenmark>(regionDenmark.EAST)
+  const [region, setRegion] = useState<regionDenmark>()
   const { data: dataEast } = useGetPrices(regionDenmark.EAST)
   const { data: dataWest } = useGetPrices(regionDenmark.WEST)
+  const [storedValue, setValue] = useLocalStorage(
+    localStorageKeys.REGION,
+    regionDenmark.EAST
+  )
+
+  useEffect(() => {
+    if (region) {
+      setValue(region)
+    }
+  }, [region, setValue])
 
   return (
     <div className={styles.home}>
       {dataEast?.length && dataWest?.length && (
         <div className={styles.home__calc}>
           <PriceCalculator
-            data={region === regionDenmark.EAST ? dataEast : dataWest}
-            region={region}
+            data={storedValue === regionDenmark.EAST ? dataEast : dataWest}
+            region={storedValue ?? regionDenmark.WEST}
             setRegion={setRegion}
           />
         </div>
