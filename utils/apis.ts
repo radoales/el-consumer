2ec5d9167779b4566925c82e2ae475942f15f3f8
radoslav <naydenov.radoslav@gmail.com>
2023-01-15 12:40:56 +0100

    remove logs

--- utils/apis.ts
+++ utils/apis.ts
@@ -2,7 +2,7 @@ export const apiRequest = async <T>(url: string): Promise<T> => {
   const resp = await fetch(url)
     .then((response) => response.json())
     .then((data) => data)
-    .catch((error) => console.log("error", error))
+    .catch(() => {})
 
   return resp
 }
